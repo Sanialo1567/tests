69d302c393be40774c18389177279af19cd6a489
Sasha Sever <alya.sever.01@mail.ru>
2020-12-09 22:23:22 +0300

    six

--- xUnit/src/test/java/CalculatorTest.java
+++ xUnit/src/test/java/CalculatorTest.java
@@ -31,58 +31,58 @@ class CalculatorTest {
     @Test
     void sumTest() {
         double expectedValue = 7.4f;
-        Assert.assertEquals(expectedValue, calculator.sum(2.4, 5), 0.1);
+        Assertions.assertEquals(expectedValue, calculator.sum(2.4, 5), 0.1);
     }
 
     @Test
     void subtractionTest() {
         Calculator calculatorOne = new Calculator();
         Calculator calculatorSecond = new Calculator();
-        Assert.assertEquals(calculatorSecond.subtraction(4.5, 2.5), calculatorOne.subtraction(8, 4));
+        Assertions.assertEquals(calculatorSecond.subtraction(4.5, 2.5), calculatorOne.subtraction(8, 4));
 
     }
 
     @Test
     void multiplicationTest() {
         double expectedValue = 40;
-        Assert.assertEquals(calculator.multiplication(8, 5), expectedValue);
+        Assertions.assertEquals(calculator.multiplication(8, 5), expectedValue);
     }
 
     @Test
     void divisionTest() {
         double expectedValue = 0.8;
-        assertEquals(calculator.division(4, 5), expectedValue);
+        Assertions.assertEquals(calculator.division(4, 5), expectedValue);
     }
 
     @Test
     public void divideByZero() {
-        Assert.assertThrows(ArithmeticException.class, () -> calculator.division(4, 0));
+        Assertions.assertThrows(ArithmeticException.class, () -> calculator.division(4, 0));
     }
 
     @Test
     void getSin() {
-        Assert.assertEquals(calculator.getSin(Math.PI), 0, 0.1);
+        Assertions.assertEquals(calculator.getSin(Math.PI), 0, 0.1);
     }
 
     @Test
     public void getNegativeSin() {
-        Assert.assertDoesNotThrow(() -> calculator.getSin(-4));
+        Assertions.assertDoesNotThrow(() -> calculator.getSin(-4));
     }
 
     @Test
     void getCos() {
-        assertEquals(calculator.getCos(Math.PI), 1, 0.1);
+        Assertions.sassertEquals(calculator.getCos(Math.PI), 1, 0.1);
     }
 
 
     @Test
     void getTan() {
-        Assert.assertEquals(calculator.getTan(0), 0, 0.1);
+        Assertions.assertEquals(calculator.getTan(0), 0, 0.1);
     }
 
     @Test
     void checkTrigonometric() {
-        Assert.assertTrue(calculator.getCos(Math.PI) * calculator.getCos(Math.PI) + calculator.getSin(Math.PI) * calculator.getSin(Math.PI) == 1);
+        Assertions.assertTrue(calculator.getCos(Math.PI) * calculator.getCos(Math.PI) + calculator.getSin(Math.PI) * calculator.getSin(Math.PI) == 1);
     }
 
 
